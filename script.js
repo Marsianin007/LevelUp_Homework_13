da34f49652ca14a6c3142b0c4cf7ba3f5af45012
Marsianin007 <chernoborodiuk20@gmail.com>
2019-03-24 18:59:17 +0200

    Second push

--- script.js
+++ script.js
@@ -1,18 +1,42 @@
 var check;
 function valid(form){
-    var pass= form.password.value;
+
+    document.getElementById("user").className = "user";
+    document.getElementById("password").className = "password";
+    
+    var pass= form.password.value.length;
+     login=document.getElementById("user");
    
     if(pass < 6 || pass > 16){
         //alert("Error");
         document.getElementById("password").className = "password_bad";
+        alert("Введите пароль нужной длины");
         check=false;
     }
     else{
         document.getElementById("password").className = "password";
         check=true;
     }
+    
+
 
     if(check){
         alert("All is good");
     }
 }
+
+function user_mistake(){
+    alert("Нельзя использовать заглавные буквы в никнейме");
+    document.getElementById("user").className = "user_bad";
+
+}
+
+function pass_mistake(){
+    alert("Нельзя использовать цифры в пароле");
+    document.getElementById("password").className = "password_bad";
+}
+   /* login.oninvalid = function(event) {
+        event.target.setCustomValidity('Username should only contain lowercase letters. e.g. john');
+        alert("GUGU");
+    }
+*/
